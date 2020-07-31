7727c7145bc5bf2477d87a42715887de7629fdb8
Gayath1 <chandulagayan@gmail.com>
2020-07-31 16:26:44 +0530

    Payment ui

--- app/src/main/java/com/example/ebooks/Activity_cart.java
+++ app/src/main/java/com/example/ebooks/Activity_cart.java
@@ -1,7 +1,10 @@
 package com.example.ebooks;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +15,18 @@ public class Activity_cart extends AppCompatActivity {
     public static TextView tv_total;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cart);
 
         tv_total =findViewById(R.id.tv_total);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Activity_payment.class);
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/example/ebooks/Activity_payment.java
@@ -0,0 +1,14 @@
+package com.example.ebooks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment);
+    }
+}
