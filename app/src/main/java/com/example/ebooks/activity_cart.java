b7bac6a72c4ba64393fc2fb00589aa8f6f964aeb
Gayath1 <chandulagayan@gmail.com>
2020-08-11 15:26:34 +0530

    Add toast in payment"Thank you"

--- app/src/main/java/com/example/ebooks/activity_cart.java
+++ app/src/main/java/com/example/ebooks/activity_cart.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,18 +16,24 @@ public class activity_cart extends AppCompatActivity {
     public static TextView tv_total;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cart);
 
-        tv_total =findViewById(R.id.tv_total);
+        tv_total = findViewById(R.id.tv_total);
+
+    }
+
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, activity_payment.class);
         startActivity(intent);
+        toastMsg("Thank you");
     }
 
 }
