3407f689ec0c298a0e343289f7a54a33f3c88ef6
Gayath1 <chandulagayan@gmail.com>
2020-10-07 22:34:11 +0530

    Finalize database

--- app/src/main/java/com/example/ebooks/CartActivity.java
+++ app/src/main/java/com/example/ebooks/CartActivity.java
@@ -1,17 +1,17 @@
 package com.example.ebooks;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ebooks.adapters.CartAdapter;
 import com.example.ebooks.models.Cart;
